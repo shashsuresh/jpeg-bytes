f9a2e147c3b406c36d8add5740464631bc229111
Shashwath <S.Suresh-3@student.tudelft.nl>
2025-02-06 19:56:51 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# JPEG - Bytes Converter
+
+A simple rust program that can recursively convert JPEG files into arrays for later use.
---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use std::{fs::{create_dir, File}, io::Write};
+
+use image::ImageReader;
+
+fn main() {
+
+    //TODO check if it exists and if it does, don't create
+    create_dir("./output").unwrap();
+    // TODO recursively convert images
+
+    let image = ImageReader::open("./images/image.jpg").unwrap().decode().unwrap();
+
+    let image = image.resize(128, 128, image::imageops::FilterType::Triangle);
+
+    let image_bytes = image.into_bytes();
+
+    let mut file = File::create("./output/image.h").unwrap();
+
+    file.write_fmt(format_args!("const unsigned char* image[] = {{")).unwrap();
+    for byte in image_bytes {
+        file.write_fmt(format_args!("{},",byte)).unwrap();
+    }
+    file.write_fmt(format_args!("}};")).unwrap();
+}
