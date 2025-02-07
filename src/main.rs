f3310de2d773966b17b6d87616c2d0c2e6687dae
Shashwath <S.Suresh-3@student.tudelft.nl>
2025-02-07 11:06:30 +0100

    working version

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{fs::{create_dir, read_dir, File, ReadDir}, io::Write, path::Path};
 
-use image::ImageReader;
+use image::{imageops::FilterType::Triangle, ImageReader};
 
 fn main() {
 
@@ -17,13 +17,19 @@ fn main() {
             let entry = image_entry.unwrap().path();
             let image = ImageReader::open(entry.clone()).unwrap().decode().unwrap();
 
-            let image = image.resize(128, 128, image::imageops::FilterType::Triangle);
+            let image = image.resize_exact(80, 80, Triangle);
         
             let image_bytes = image.into_bytes();
 
-            file.write_fmt(format_args!("const unsigned char {} = {{", entry.to_str().unwrap().strip_suffix(".jpg").unwrap().strip_prefix("./images/").unwrap())).unwrap();
+            file.write_fmt(format_args!("const unsigned char {}[] = {{", entry.to_str().unwrap().strip_suffix(".jpg").unwrap().strip_prefix("./images/").unwrap())).unwrap();
+            let mut elem_ctr = 0;
             for byte in image_bytes {
-                file.write_fmt(format_args!("{},",byte)).unwrap();
+                file.write_fmt(format_args!("{}, ",byte)).unwrap();
+                elem_ctr+=1;
+                if elem_ctr == 45 {
+                    file.write_fmt(format_args!("\n")).unwrap();
+                    elem_ctr = 0;
+                }
             }
             file.write_fmt(format_args!("}};\n")).unwrap();
         }
